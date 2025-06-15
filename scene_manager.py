0a598ec6fa40d3d6fcaf678d98b97379a7708e2d
Abdulaziz334 <abdulaziznajimov334@gmail.com>
2025-06-15 17:43:56 +0500

    Update scene_manager.py

--- scene_manager.py
+++ scene_manager.py
@@ -25,3 +25,18 @@ class SceneManager:
         if name in self.scenes:
             self.current_scene = self.scenes[name]
         return self.current_scene
+class Scene:
+    def __init__(self, name):
+        self.name = name
+        self.objects = []
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+class SceneManager:
+    def __init__(self):
+        self.current_scene = None
+
+    def create_scene(self, name):
+        self.current_scene = Scene(name)
+        return self.current_scene
