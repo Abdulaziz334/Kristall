ee803adfa60d9963722627971a03f855112bd672
Abdulaziz334 <abdulaziznajimov334@gmail.com>
2025-06-15 17:47:59 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from graphics_engine import GraphicsEngine
+from audio_engine import AudioEngine
+from ui_manager import UIManager
+from ai_core import AIEngine
+from physics_core import PhysicsEngine
+from scene_manager import SceneManager
+
+# Modul obyektlari
+graphics = GraphicsEngine()
+audio = AudioEngine()
+ui = UIManager()
+ai = AIEngine()
+physics = PhysicsEngine()
+scene = SceneManager()
+
+# Test
+scene.create_scene("menu")
+ui.add_button("Start")
+graphics.add_object("Logo.png")
+audio.play_sound("intro.wav")
+print(ai.process_input("move"))
+print("Gravitatsiya:", physics.apply_gravity(1, 5))
