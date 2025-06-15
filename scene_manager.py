0b143b722986febc2d806e4db3ffa26e29333252
Abdulaziz334 <abdulaziznajimov334@gmail.com>
2025-06-15 17:26:33 +0500

    Create scene_manager.py

---
+++ scene_manager.py
@@ -0,0 +1,27 @@
+# scene_manager.py
+
+class Scene:
+    def __init__(self, name):
+        self.name = name
+        self.objects = []
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def remove_object(self, obj):
+        self.objects.remove(obj)
+
+class SceneManager:
+    def __init__(self):
+        self.scenes = {}
+        self.current_scene = None
+
+    def create_scene(self, name):
+        scene = Scene(name)
+        self.scenes[name] = scene
+        return scene
+
+    def switch_scene(self, name):
+        if name in self.scenes:
+            self.current_scene = self.scenes[name]
+        return self.current_scene
