1089e769a467b497eed7951a4f0fcb0c39132e23
Abdulaziz334 <abdulaziznajimov334@gmail.com>
2025-06-15 17:13:51 +0500

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -1,3 +1,9 @@
 def start_render_loop():
     print("🖼 Grafik dvijok ishga tushmoqda...")
     # Grafik interfeyslar shu yerda boshlab ishlaydi
+KristallEngine/
+├── ai_core.py
+├── engine.py
+├── physics.py
+├── renderer.py
+└── README.md
