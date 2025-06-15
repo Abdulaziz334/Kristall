45998b50592e71164dc07129c994560850452232
Abdulaziz334 <abdulaziznajimov334@gmail.com>
2025-06-15 19:12:09 +0500

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,10 @@
+def main_menu():
+    print("=== KristallEngine ===")
+    print("1. Dronga buyurtma berish")
+    print("2. Bazani tanlash")
+    print("3. To‘lov qilish")
+    print("4. Sozlamalar")
+    print("0. Chiqish")
+
+if __name__ == "__main__":
+    main_menu()
