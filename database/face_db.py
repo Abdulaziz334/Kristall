24dc27d8ca2c6fb45ab81ffc15524b9eda788b78
Abdulaziz334 <abdulaziznajimov334@gmail.com>
2025-06-17 17:24:56 +0500

    Create database/face_db.py

---
+++ database/face_db.py
@@ -0,0 +1,6 @@
+from core.models import Product
+
+fake_products = [
+    Product(id=1, name="Choynak", weight_kg=0.8, price=120000, base_id=1),
+    Product(id=2, name="Telefon qutisi", weight_kg=0.5, price=900000, base_id=2),
+]
