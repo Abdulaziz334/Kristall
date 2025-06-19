ca804a63b750e3cf5504458e4a6e030d2a3d4709
Abdulaziz334 <abdulaziznajimov334@gmail.com>
2025-06-19 20:13:09 +0500

    Update test_user_module.py

--- tests/test_user_module.py
+++ tests/test_user_module.py
@@ -7,3 +7,11 @@ print("Yangi foydalanuvchi:", user)
 # Foydalanuvchini telefon orqali qidirish
 found_user = get_user_by_phone("+998901234567")
 print("Topilgan foydalanuvchi:", found_user)
+from user_module import register_user, add_order_history, get_user_history
+
+user = register_user("Abdulaziz", "Toshkent")
+print("Foydalanuvchi:", user)
+
+add_order_history(user["user_id"], "order123")
+tarix = get_user_history(user["user_id"])
+print("Buyurtma tarixi:", tarix)
