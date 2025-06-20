02045bb705823e2500cbb50e13670a38a268fca2
Abdulaziz334 <abdulaziznajimov334@gmail.com>
2025-06-20 10:13:30 +0500

    Update physics.py

--- physics.py
+++ physics.py
@@ -1,9 +1,9 @@
 def load_physics():
     print("🌐 Fizika dvijogi yuklanmoqda...")
     # Asosiy fizika formulalari yoki tizimlar shu yerga yoziladi
-KristallEngine/
-├── ai_core.py
-├── engine.py
-├── physics.py
-├── renderer.py
-└── README.md
+#KristallEngine/
+#├── ai_core.py
+#├── engine.py
+#├── physics.py
+#├── renderer.py
+#└── README.md
